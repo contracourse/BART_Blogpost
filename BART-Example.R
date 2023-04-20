621a2f40a851ea861dfb87f42d3163cb9802cccf
contracourse <kai.bruening@cbs-mail.de>
2023-04-20 15:00:18 +0200

    init commit

---
+++ BART-Example.R
@@ -0,0 +1,42 @@
+library(bartMachine)
+data(automobile)
+automobile = na.omit(automobile)
+y <- automobile$log_price
+X <- automobile; X$log_price <- NULL
+
+bart_machine <- bartMachine(X, y)
+bart_machine    
+# Note that the “p-val for shapiro-wilk test of normality of residuals” is marginally less than
+#5%. Thus we conclude that the noise of Equation 1 is not normally distributed.
+
+k_fold_cv(X, y, k_folds = 10)
+rmse_by_num_trees(bart_machine, 
+                  tree_list=c(seq(25, 75, by=5)),
+                  num_replicates=3)
+
+bart_machine_cv <- bartMachineCV(X, y)
+
+k_fold_cv(X, y, k_folds = 10, k = 2, nu = 3, q = 0.9, num_trees = 200)
+
+predict(bart_machine_cv, X[1 : 7, ])
+
+check_bart_error_assumptions(bart_machine_cv)
+
+plot_convergence_diagnostics(bart_machine_cv)
+
+calc_credible_intervals(bart_machine_cv, X[100, ], ci_conf = 0.95)
+
+calc_prediction_intervals(bart_machine_cv, X[100, ], pi_conf = 0.95)
+
+plot_y_vs_yhat(bart_machine_cv, credible_intervals = TRUE)
+plot_y_vs_yhat(bart_machine_cv, prediction_intervals = TRUE)
+plot_convergence_diagnostics(bart_machine_cv)
+
+investigate_var_importance(bart_machine_cv, num_replicates_for_avg = 20)
+
+cov_importance_test(bart_machine_cv, covariates = c("width"))
+cov_importance_test(bart_machine_cv, covariates = c("body_style"))
+cov_importance_test(bart_machine_cv, covariates = c("width",
+"curb_weight", "city_mpg", "length", "horsepower", "body_style",
+"engine_size", "highway_mpg", "peak_rpm", "normalized_losses"))
+cov_importance_test(bart_machine_cv)
---
+++ xgboost.py
@@ -0,0 +1,20 @@
+import numpy as np
+import xgboost
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from scipy.stats import pearsonr
+
+data = load_boston()
+X = data['data']
+y = data['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+xgb = xgboost.XGBRegressor()
+xgb.fit(X_train, y_train)
+y_pred = xgb.predict(X_test)
+
+print(f'RMSE:{np.sqrt(mean_squared_error(y_test, y_pred))}')
+print(f'r2: {r2_score(y_test, y_pred)}')
+print(f'pearsonr: {pearsonr(y_test, y_pred)}')
