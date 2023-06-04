5dceddce308361e2c192735bc776d24bbdfc55e5
FurryDwarf <r776or3s5@mozmail.com>
2023-06-04 15:27:19 +0200

    final

--- blogpost-bartMachine.R
+++ blogpost-bartMachine.R
@@ -7,7 +7,7 @@ options(java.parameters = "-Xmx2g")
 set_bart_machine_num_cores(3)
 
 tibble::tibble(a = 1)
-api_key <- "yourapikey"
+api_key <- "your-api-key"
 fredr_set_key(api_key)
 
 cpi_df <- fredr(
@@ -32,15 +32,16 @@ df <- purrr::pmap_dfr(
 
 
 df <- as.data.table(df)
-df1 <- rbind(df, cpi_df)
+df1 <- rbind(df, cpi_df) 
 df1 <- df1[ date %between% c("2004-01-01","2022-12-31")]
 which(is.na(df1), arr.ind=TRUE) # checking if there are NA's in the dataframe
 # df_a <- df1[,2:3]
 
 data_frame <- pivot_wider(df1, names_from = "series_id",
 values_from = "value")
+str(data_frame)
 data_frame <- data_frame[,4:10] # delete the dates column
-which(is.na(data_frame), arr.ind=TRUE) # checking if there are NA's in the dataframe
+which(is.na(df1), arr.ind=TRUE) # checking if there are NA's in the dataframe
 
 library(caret)
 y <- data_frame$UNRATE
@@ -74,43 +75,28 @@ rmse_by_num_trees(bart_machine,
                   tree_list=c(seq(15, 75, by=5)),
                   num_replicates=3)
 
-bart_machine <- bartMachine(df_train, y_train, num_trees=35)
+bart_machine <- bartMachine(df_train, y_train, num_trees=20)
 plot_convergence_diagnostics(bart_machine)
 
 check_bart_error_assumptions(bart_machine)
 
 plot_y_vs_yhat(bart_machine, prediction_intervals = TRUE)
 
-par(mfrow = c(1,1),     # 2 rows, 1 column
+par(mfrow = c(2,1),     # 2 rows, 1 column
     mar = c(4,4,2,1))
-plot_y_vs_yhat(bart_machine, credible_intervals = TRUE)
-plot_y_vs_yhat(bart_machine, Xtest=df_test, ytest=y_test, credible_intervals = TRUE, interval_confidence_level = 0.89)
+plot_y_vs_yhat(bart_machine, prediction_intervals = TRUE, interval_confidence_level = 0.90)
+plot_y_vs_yhat(bart_machine, Xtest=df_test, ytest=y_test, credible_intervals = TRUE, interval_confidence_level = 0.90)
 
-
-rmse <- function(x, y) sqrt(mean((x - y)^2))
-rsq <- function(x, y) summary(lm(y~x))$r.squared
 y_pred <- predict(bart_machine, df_test)
-paste('r2:', rsq(y_test, y_pred)) # the R-squared y-test fit with predicted 
-paste('rmse:', rmse(y_test, y_pred))
+
+summary(lm(y_test~y_pred))$r.squared
+sqrt(mean((y_test - y_pred)^2))
 cor.test(y_test, y_pred, method=c("pearson"))
 
 # Plot the importance plot
 investigate_var_importance(bart_machine, num_replicates_for_avg = 20)
-pd_plot(bart_machine, j = "UNRATE") # Investigate the most important feature in the PD plot
-cov_importance_test(bart_machine, covariates = "UNRATE")
+pd_plot(bart_machine, j = "EFFR") # Investigate the most important feature in the PD plot
 
-# Example aus BART
-set.seed(11)
-n = 200
-p = 5
-X = data.frame(matrix(runif(n * p), ncol = p))
-y = 10 * sin(pi* X[ ,1] * X[,2]) +20 * (X[,3] -.5)^2 + 10 * X[ ,4] + 5 * X[,5] + rnorm(n)
-##build BART regression model
-bart_machine = bartMachine(X, y)
-#get posterior distribution
-posterior = bart_machine_get_posterior(bart_machine, X)
-print(posterior$y_hat)
-plot_convergence_diagnostics(bart_machine)
 
 
 
